180ee6e2e8c8e611dfc8d805b99475c54a55b931
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-04-10 16:53:01 +0100

    stage 1

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans|Saira+Semi+Condensed" rel="stylesheet">
+    <title>HNG Stage 1</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="aside-bar">
+            <div class="side-content">
+                <div class="avatar-holder">
+                    <img src="assets/avataaars.png" alt="">
+                </div>
+
+                <div class="dev-info">
+                    <h1>Idakwoji Theophilus</h1>
+
+                    <h2>Software Developer </h2>
+                </div>
+
+                <div class="dev-links">
+                    <div class="circle">
+                       <a href="https://pietrosparks.github.io"> <img src="assets/github.png" alt="github"></a>
+                    </div>
+                    <div class="circle">
+                       <a href="https://twitter.com/txheo"><img src="assets/twitter-logo.png" alt="twitter"></a> 
+                    </div>
+                    <div class="circle">
+                    <a href="pietrosparks@gmail.com"><img src="assets/mail.png" alt="mail"></a>
+     
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="contents">
+            <div class="content-intro">
+                <h1>Welcome to the HNG Internship 4</h1>
+                <p>The time on this day </p>
+            </div>
+
+            <div class="content-time">
+                <p><?php echo date('h:i:sa') ?></p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
